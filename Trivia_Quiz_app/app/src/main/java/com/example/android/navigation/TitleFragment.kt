ea6eafbebf9367c144fb4719bd823487d395e6e9
Mamun <mamunmiah193gsc@gmail.com>
2022-06-21 23:12:50 +0600

    Let's navigate already

--- Trivia_Quiz_app/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ Trivia_Quiz_app/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -22,6 +22,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
@@ -29,6 +30,10 @@ class TitleFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_title, container, false)
+
+        binding.playButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+        )
         return binding.root
     }
 }
